5ee861c946fc4abdd5716d543db4ea67f151e58c
swiftdropshippingcompany-ui <swiftdropshippingcompany@gmail.com>
2025-08-30 02:24:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-app = Flask('')
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "I'm alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-# keep alive and run the bot
-
-keep_alive()
-bot.run(os.environ["DISCORD_BOT_TOKEN"])
