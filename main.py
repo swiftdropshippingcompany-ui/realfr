3fa17b9759ec4b115837b318616e902678b1a748
swiftdropshippingcompany-ui <swiftdropshippingcompany@gmail.com>
2025-08-30 02:23:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -908,21 +908,4 @@ async def on_message(message: discord.Message):
         await message.channel.send(random.choice(lupus_responses))
     
 
-# Flask app for keeping the bot alive
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "I'm alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-# keep alive and run the bot
-
-keep_alive()
 bot.run(os.environ["DISCORD_BOT_TOKEN"])
